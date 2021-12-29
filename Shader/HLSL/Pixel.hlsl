a4af5ab41604b490fe2c8c3f480af493bb8cdd29
봉 봉 <ekffjaos553@naver.com>
2021-12-29 14:50:35 +0900

    211229_이미지출력수정_애니메이션출력

--- Shader/HLSL/Pixel.hlsl
+++ Shader/HLSL/Pixel.hlsl
@@ -6,5 +6,8 @@ Texture2D Texture;
 
 float4 Shader(Output output) : SV_Target //함수 옆의 시멘틱은 반환값이 곧 SV_Target을 의미한다는 문구
 {
-    return Texture.Load(int3(output.TexCoord, 0));
+    float4 color = Texture.Load(int3(output.TexCoord, 0));
+    if(color.a !=1)
+        discard;
+    return color;
 }
